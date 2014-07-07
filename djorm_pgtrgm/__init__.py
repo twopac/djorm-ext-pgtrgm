9489468a40e65cae3f208f3dec1b631ca95a1d4c
twopac <twopac@bk.ru>
2014-07-08 00:09:57 +0400

    Delete postgis dependencies

--- djorm_pgtrgm/__init__.py
+++ djorm_pgtrgm/__init__.py
@@ -4,9 +4,8 @@ from django.db import models
 from django.db.models.fields import Field, subclassing
 from django.db.models.query import QuerySet
 from django.db.models.sql.constants import QUERY_TERMS
-from django.contrib.gis.db.models.sql.query import ALL_TERMS
 
-db_backends_allowed = ('postgresql', 'postgis')
+db_backends_allowed = ('postgresql')
 
 
 def get_prep_lookup(self, lookup_type, value):
@@ -50,12 +49,6 @@ if backend_allowed:
         QUERY_TERMS.add('similar')
     else:
         QUERY_TERMS['similar'] = None
-
-    if backend_allowed == 'postgis':
-        if isinstance(ALL_TERMS, set):
-            ALL_TERMS.add('similar')
-        else:
-            ALL_TERMS['similar'] = None
 
     connection.operators['similar'] = "%%%% %s"
 
